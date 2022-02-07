149db8d25338b7b6524b7d7f229af948f1a99fa1
yael-vinker <yaelvi116@gmail.com>
2022-02-07 16:47:41 +0200

    formatting code

--- display_results.py
+++ display_results.py
@@ -2,7 +2,7 @@ import re
 import torch
 import pydiffvg
 import argparse
-from IPython.display import display, clear_output
+from IPython.display import display
 from IPython.display import Image as Image_colab
 import matplotlib.pyplot as plt
 import imageio
@@ -14,13 +14,16 @@ from PIL import Image
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--target_file", type=str, help="target image file, located in <target_images>")
+parser.add_argument("--target_file", type=str,
+                    help="target image file, located in <target_images>")
 args = parser.parse_args()
 
 
 def read_svg(path_svg, multiply=False):
-    device = torch.device("cuda" if (torch.cuda.is_available() and torch.cuda.device_count() > 0) else "cpu")
-    canvas_width, canvas_height, shapes, shape_groups = pydiffvg.svg_to_scene(path_svg)
+    device = torch.device("cuda" if (
+        torch.cuda.is_available() and torch.cuda.device_count() > 0) else "cpu")
+    canvas_width, canvas_height, shapes, shape_groups = pydiffvg.svg_to_scene(
+        path_svg)
     if multiply:
         canvas_width *= 2
         canvas_height *= 2
@@ -28,17 +31,21 @@ def read_svg(path_svg, multiply=False):
             path.points *= 2
             path.stroke_width *= 2
     _render = pydiffvg.RenderFunction.apply
-    scene_args = pydiffvg.RenderFunction.serialize_scene(canvas_width, canvas_height, shapes, shape_groups)
-    img = _render(canvas_width, # width
-                canvas_height, # height
-                2,   # num_samples_x
-                2,   # num_samples_y
-                0,   # seed
-                None,
-                *scene_args)
-    img = img[:, :, 3:4] * img[:, :, :3] + torch.ones(img.shape[0], img.shape[1], 3, device = device) * (1 - img[:, :, 3:4])
+    scene_args = pydiffvg.RenderFunction.serialize_scene(
+        canvas_width, canvas_height, shapes, shape_groups)
+    img = _render(canvas_width,  # width
+                  canvas_height,  # height
+                  2,   # num_samples_x
+                  2,   # num_samples_y
+                  0,   # seed
+                  None,
+                  *scene_args)
+    img = img[:, :, 3:4] * img[:, :, :3] + \
+        torch.ones(img.shape[0], img.shape[1], 3,
+                   device=device) * (1 - img[:, :, 3:4])
     img = img[:, :, :3]
     return img
+
 
 abs_path = os.path.abspath(os.getcwd())
 target_path = f"{abs_path}/target_images/{args.target_file}"
@@ -56,7 +63,7 @@ display(sketch_res)
 p = re.compile("_best")
 best_sketch_dir = ""
 for m in p.finditer(svg_files[0]):
-    best_sketch_dir += svg_files[0][0 : m.start()]
+    best_sketch_dir += svg_files[0][0: m.start()]
 
 
 sketches = []
@@ -66,7 +73,7 @@ if os.path.exists(f"{cur_path}/config.npy"):
     inter = config["save_interval"]
     loss_eval = np.array(config['loss_eval'])
     inds = np.argsort(loss_eval)
-    intervals = list(range(0, (inds[0] + 1) * inter,inter))
+    intervals = list(range(0, (inds[0] + 1) * inter, inter))
     for i_ in intervals:
         path_svg = f"{cur_path}/svg_iter{i_}.svg"
         sketch = read_svg(path_svg, multiply=True).cpu().numpy()
--- painterly_rendering.py
+++ painterly_rendering.py
@@ -17,17 +17,17 @@ from torch.cuda.amp import autocast
 from PIL import Image
 from models.loss import Loss
 from models.painter_params import Painter, PainterOptimizer
-import sys 
+import sys
 import PIL
 
 
 def load_renderer(args, target_im=None, mask=None):
     renderer = Painter(num_strokes=args.num_paths, args=args,
-                        num_segments=args.num_segments,
-                        imsize=args.image_scale,
-                        device=args.device,
-                        target_im=target_im,
-                        mask=mask)
+                       num_segments=args.num_segments,
+                       imsize=args.image_scale,
+                       device=args.device,
+                       target_im=target_im,
+                       mask=mask)
     renderer = renderer.to(args.device)
     return renderer
 
@@ -35,8 +35,10 @@ def load_renderer(args, target_im=None, mask=None):
 def get_target(args):
     target = Image.open(args.target)
     if target.mode == "RGBA":
-        new_image = Image.new("RGBA", target.size, "WHITE")  # Create a white rgba background
-        new_image.paste(target, (0, 0), target)  # Paste the image on the background.
+        # Create a white rgba background
+        new_image = Image.new("RGBA", target.size, "WHITE")
+        # Paste the image on the background.
+        new_image.paste(target, (0, 0), target)
         target = new_image
     target = target.convert("RGB")
     masked_im, mask = utils.get_mask_u2net(args, target)
@@ -47,16 +49,18 @@ def get_target(args):
 
     transforms_ = []
     if target.size[0] != target.size[1]:
-        transforms_.append(transforms.Resize((args.image_scale, args.image_scale), interpolation=PIL.Image.BICUBIC))
+        transforms_.append(transforms.Resize(
+            (args.image_scale, args.image_scale), interpolation=PIL.Image.BICUBIC))
     else:
-        transforms_.append(transforms.Resize(args.image_scale, interpolation=PIL.Image.BICUBIC))
+        transforms_.append(transforms.Resize(
+            args.image_scale, interpolation=PIL.Image.BICUBIC))
         transforms_.append(transforms.CenterCrop(args.image_scale))
     transforms_.append(transforms.ToTensor())
     data_transforms = transforms.Compose(transforms_)
     target_ = data_transforms(target).unsqueeze(0).to(args.device)
     return target_, mask
 
-            
+
 def main(args):
     loss_func = Loss(args)
     inputs, mask = get_target(args)
@@ -83,16 +87,19 @@ def main(args):
         start = time.time()
         optimizer.zero_grad_()
         sketches = renderer.get_image().to(args.device)
-        losses_dict = loss_func(sketches, inputs.detach(), renderer.get_color_parameters(), renderer, counter, optimizer)
+        losses_dict = loss_func(sketches, inputs.detach(
+        ), renderer.get_color_parameters(), renderer, counter, optimizer)
         loss = sum(list(losses_dict.values()))
         loss.backward()
         optimizer.step_()
         if epoch % args.save_interval == 0:
-            utils.plot_batch(inputs, sketches, args, counter, use_wandb=args.use_wandb, title=f"iter{epoch}.jpg")
+            utils.plot_batch(inputs, sketches, args, counter,
+                             use_wandb=args.use_wandb, title=f"iter{epoch}.jpg")
             renderer.save_svg(args.output_dir, f"svg_iter{epoch}")
         if epoch % args.eval_interval == 0:
             with torch.no_grad():
-                losses_dict_eval = loss_func(sketches, inputs, renderer.get_color_parameters(), renderer.get_points_parans(), counter, optimizer, mode="eval")
+                losses_dict_eval = loss_func(sketches, inputs, renderer.get_color_parameters(
+                ), renderer.get_points_parans(), counter, optimizer, mode="eval")
                 loss_eval = sum(list(losses_dict_eval.values()))
                 configs_to_save["loss_eval"].append(loss_eval.item())
                 for k in losses_dict_eval.keys():
@@ -101,51 +108,55 @@ def main(args):
                     configs_to_save[k].append(losses_dict_eval[k].item())
                 if args.clip_fc_loss_weight:
                     if losses_dict_eval["fc"].item() < best_fc_loss:
-                        best_fc_loss = losses_dict_eval["fc"].item() / args.clip_fc_loss_weight
+                        best_fc_loss = losses_dict_eval["fc"].item(
+                        ) / args.clip_fc_loss_weight
                         best_iter_fc = epoch
-                print(f"eval iter[{epoch}/{args.num_iter}] loss[{loss.item()}] time[{time.time() - start}]")
-                
+                print(
+                    f"eval iter[{epoch}/{args.num_iter}] loss[{loss.item()}] time[{time.time() - start}]")
+
                 cur_delta = loss_eval.item() - best_loss
                 if abs(cur_delta) > min_delta:
                     if cur_delta < 0:
                         best_loss = loss_eval.item()
                         best_iter = epoch
                         terminate = False
-                        utils.plot_batch(inputs, sketches, args, counter, use_wandb=args.use_wandb, title="best_iter.jpg")
+                        utils.plot_batch(
+                            inputs, sketches, args, counter, use_wandb=args.use_wandb, title="best_iter.jpg")
                         renderer.save_svg(args.output_dir, "best_iter")
 
                 if args.use_wandb:
                     wandb.run.summary["best_loss"] = best_loss
                     wandb.run.summary["best_loss_fc"] = best_fc_loss
-                    wandb_dict = {"delta": cur_delta, "loss_eval": loss_eval.item()}
+                    wandb_dict = {"delta": cur_delta,
+                                  "loss_eval": loss_eval.item()}
                     for k in losses_dict_eval.keys():
                         wandb_dict[k + "_eval"] = losses_dict_eval[k].item()
                     wandb.log(wandb_dict, step=counter)
-                
+
                 if abs(cur_delta) <= min_delta:
                     if terminate:
                         break
                     terminate = True
 
-                    
         if counter == 0 and args.attention_init:
-            utils.plot_atten(renderer.get_attn(), renderer.get_thresh(), inputs, renderer.get_inds(), 
-                                args.use_wandb, "{}/{}.jpg".format(args.output_dir, "attention_map"), 
-                                args.saliency_model, args.display_logs)
-
+            utils.plot_atten(renderer.get_attn(), renderer.get_thresh(), inputs, renderer.get_inds(),
+                             args.use_wandb, "{}/{}.jpg".format(
+                                 args.output_dir, "attention_map"),
+                             args.saliency_model, args.display_logs)
 
         if args.use_wandb:
             wandb_dict = {"loss": loss.item(), "lr": optimizer.get_lr()}
             for k in losses_dict.keys():
                 wandb_dict[k] = losses_dict[k].item()
             wandb.log(wandb_dict, step=counter)
-        
+
         counter += 1
 
     renderer.save_svg(args.output_dir, "final_svg")
     path_svg = os.path.join(args.output_dir, "best_iter.svg")
-    utils.log_sketch_summary_final(path_svg, args.use_wandb, args.device, best_iter, best_loss, "best total")
-    
+    utils.log_sketch_summary_final(
+        path_svg, args.use_wandb, args.device, best_iter, best_loss, "best total")
+
     return configs_to_save
 
 
